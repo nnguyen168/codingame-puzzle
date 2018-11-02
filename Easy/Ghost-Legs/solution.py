4f1932c097dfc041b1755dcd8850bcad7543e273
Nguyen Hoai Nam <nguyenhoainam2k11@gmail.com>
2018-11-02 11:22:26 +0100

    Update solution.py
    Add comments

--- Easy/Ghost-Legs/solution.py
+++ Easy/Ghost-Legs/solution.py
@@ -5,9 +5,9 @@ import math
 # the standard input according to the problem statement.
 
 w, h = [int(i) for i in input().split()]
-top = []
-bottom = []
-legs = []
+top = [] # a list of start points
+bottom = [] # a list of end points
+legs = [] # a list of list where each sublist is a line of Ghost Legs diagram
 for i in range(h):
     line = input()
     if i == 0:
@@ -17,22 +17,23 @@ for i in range(h):
         bottom.append(line)
         bottom = list(bottom[0].replace(' ', ''))
     else:
-        line = line.replace('|--|', '|--  |')
+        # here we make an ugly trick to get the transition we want as '|--|  |' -> ['|--', '|', '|']
+        line = line.replace('|--|', '|--  |') 
         line = line.split('  ')
         legs.append(line)
 
 # Write an action using print
 # To debug: print("Debug messages...", file=sys.stderr)
-result = []
+result = [] # a list of connected pairs
 for i in range(len(top)):
     pair = ''
     pair += top[i]
-    end = i
+    end = i # assume that there is no connection and it goes straightly to the bottom
     for j in range(len(legs)):
-        if len(legs[j][end]) == 3:
+        if len(legs[j][end]) == 3: # if there is connection between this leg and the one after it
             end = end + 1
         elif end != 0:
-            if len(legs[j][end-1]) == 3:
+            if len(legs[j][end-1]) == 3: # if there is connection between this leg and the one before it
                 end = end - 1
     pair += bottom[end]
     result.append(pair)
